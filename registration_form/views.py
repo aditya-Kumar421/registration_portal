ddcf48b081f194623033a68f884e919146391641
Aditya <ak.adityakumar421@gmail.com>
2023-12-09 19:55:28 +0530

    exclude comments

--- registration_form/views.py
+++ registration_form/views.py
@@ -26,26 +26,3 @@ class RegistrationList(APIView):
             return Response({'success': True},{"message": "Registration successful"}, status=status.HTTP_201_CREATED)
         return Response({'error': "reCAPTCHA verification failed"}, status=400)
 
-        # serializer.save()
-        # return Response(
-        #     {"message": "Registration successful"},
-        #     status=status.HTTP_201_CREATED
-        # )
-
-
-# class captchaView(APIView):
-#     def post(self, request):
-#         captcha_token = request.data.get('captcha', '')
-#         data = {
-#             'secret': settings.RECAPTCHA_PRIVATE_KEY,
-#             'response': captcha_token
-#         }
-
-        # response = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
-        # result = response.json()
-
-        # if result['success']:
-        #     return Response({'success': True})
-        # return Response({'error': "reCAPTCHA verification failed"}, status=400)
-
-
