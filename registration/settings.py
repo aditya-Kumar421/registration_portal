65c3a89355af2c93875afc616166568fcaca871d
Aditya <ak.adityakumar421@gmail.com>
2023-12-09 23:05:59 +0530

    secret key location

--- registration/settings.py
+++ registration/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-dt%(%q0%i7t2ks=fte$jj6wa%&u@(2&^-jyz=#$w()s*)7r5_x'
+# SECRET_KEY = config('SECRET_KEY')
 
 
 ALLOWED_HOSTS = ["techbuzz.onrender.com"]
@@ -81,7 +82,7 @@ WSGI_APPLICATION = 'registration.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / config('DB_NAME'),#
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
