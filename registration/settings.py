a7731a07f27dd1e08a89e8298c4016134e61eec5
Aditya <ak.adityakumar421@gmail.com>
2023-12-10 00:15:06 +0530

    change css of admin

--- registration/settings.py
+++ registration/settings.py
@@ -124,12 +124,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = 'staticfiles'
+MEDIA_ROOT = BASE_DIR /'media'
+MEDIA_URL = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+CSRF_TRUSTED_ORIGINS = ['https://techbuzz.onrender.com']
 #recaptcha : 
 RECAPTCHA_PUBLIC_KEY = '6Lcd2CMpAAAAAKLqwdxjTgnWwzSgAGEgtl0BVOng'
 RECAPTCHA_PRIVATE_KEY = '6Lcd2CMpAAAAAKHPuV4UCQ2I5akmbKTvu36XZ5lm'
--- registration/urls.py
+++ registration/urls.py
@@ -2,7 +2,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.urls import re_path as url
+from django.views.static import serve
+
+from django.conf import settings
 urlpatterns = [
+    url(r'media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    url(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('registrationPortalDatabase/', admin.site.urls),
     path('api/', include('registration_form.urls')),
     path('registrationPortalDatabase/defender', include('defender.urls')),
