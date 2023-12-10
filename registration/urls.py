d53c3a0d2d1d7746aaa50bc45179b59bba35fd35
Aditya <ak.adityakumar421@gmail.com>
2023-12-10 10:01:46 +0530

    change admin name

--- registration/urls.py
+++ registration/urls.py
@@ -6,9 +6,11 @@ from django.urls import re_path as url
 from django.views.static import serve
 
 from django.conf import settings
+
+
 urlpatterns = [
     url(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
-    path('registrationPortalDatabase/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/', include('registration_form.urls')),
     path('registrationPortalDatabase/defender', include('defender.urls')),
 ]
