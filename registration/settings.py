63bafa8e7b7b6f1cc7ae5fe8cf82cd636556f78d
Aditya <ak.adityakumar421@gmail.com>
2023-12-10 10:57:07 +0530

    change url

--- registration/settings.py
+++ registration/settings.py
@@ -75,16 +75,6 @@ TEMPLATES = [
     },
 ]
 
-CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://localhost:6379/1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
-    }
-}
-
 WSGI_APPLICATION = 'registration.wsgi.application'
 
 
--- registration/urls.py
+++ registration/urls.py
@@ -2,14 +2,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from django.urls import re_path as url
-from django.views.static import serve
-
-from django.conf import settings
-
 
 urlpatterns = [
-    url(r'static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('api/', include('registration_form.urls')),
     path('registrationPortalDatabase/defender', include('defender.urls')),
